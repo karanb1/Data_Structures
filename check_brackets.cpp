cf9ba18cbebf04dca237d23d673d5b656c01509d
Akshit Aggarwal <akshitx10@gmail.com>
2019-08-30 19:02:27 +0530

    Week 1 Problem 1

---
+++ check_brackets.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Bracket {
+    Bracket(char type, int position):
+        type(type),
+        position(position)
+    {}
+
+    bool Matchc(char c) {
+        if (type == '[' && c == ']')
+            return true;
+        if (type == '{' && c == '}')
+            return true;
+        if (type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+};
+
+int main() {
+    string text;
+    getline(cin, text);
+
+    stack <Bracket> opening_brackets_stack;
+    for (int position = 0; position < text.length(); ++position) {
+        char next = text[position];
+
+        if (next == '(' || next == '[' || next == '{') {
+            opening_brackets_stack.push(Bracket(next, position));
+        }
+
+        if (next == ')' || next == ']' || next == '}') {
+            if(opening_brackets_stack.empty())
+            {
+                cout<<position + 1<<'\n';
+                return 0;
+            }
+            else if(!opening_brackets_stack.top().Matchc(next))
+            {
+                cout<<position + 1<<'\n';
+                return 0;
+            }
+            else opening_brackets_stack.pop();
+        }
+    }
+
+    if(opening_brackets_stack.empty()) cout<<"Success\n";
+    else{
+        cout<<opening_brackets_stack.top().position + 1<<'\n';
+    }
+    return 0;
+}
