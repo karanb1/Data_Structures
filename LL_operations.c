38321177bbbc8691b56e6885cecd7069889f2c44
Akshit Aggarwal <akshitx10@gmail.com>
2019-10-19 14:40:00 +0530

    Linked List Linear

--- LL_operations.c
+++ LL_operations.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-int count = 0;
 struct node{
 	int data;
 	struct node *next;
@@ -13,7 +12,6 @@ void insertFront(int a)
 	{
 		printf("Overflow!"); return;
 	}
-	count++;
 	n -> data = a;
 	if(start == NULL)
 		n -> next = NULL;
@@ -21,30 +19,51 @@ void insertFront(int a)
 		n -> next = start;
 	start = n;
 }
-
 void insertRear(int a)
 {
 	struct node *n = (struct node*)malloc(sizeof(struct node));
 	if(n == NULL)
-	{
-		printf("Overflow!"); return;
-	}
-	count++;
-	struct node *trv = start;
+	{ printf("Overflow!"); return;}
 	n -> data = a;
+	struct node *trv = start;
 	if(start == NULL)
-		n -> next = NULL, start = n;
-	else 
+		start = n;
+	else
 	{
-		while(trv != NULL)
+		while(trv -> next != NULL)
 			trv = trv -> next;
 		trv -> next = n;
-		n -> next = NULL;
 	}
+	n -> next = NULL;
 }
 void insertPos(int a, int pos);
-void deleteFront();
-void deleteRear();
+void deleteFront()
+{
+	if(start == NULL)
+	{printf("List is Empty!\n"); return;}
+	struct node *temp = start, *trv = start -> next;
+	if(trv == NULL)
+		start = NULL;
+	else
+		start = trv;
+	free(temp);
+}
+void deleteRear()
+{
+	if(start == NULL)
+	{printf("List is Empty!\n"); return;}
+	struct node *temp = start, *trv = start -> next;
+	if(trv == NULL)
+		start = NULL;
+	else
+	{
+		while((trv -> next) -> next != NULL)
+			trv = trv -> next;
+		temp = trv -> next;
+		trv -> next = NULL;
+	}
+	free(temp);
+}
 void deletePos(int pos);
 void traverse()
 {
@@ -78,12 +97,12 @@ int main()
 /*			case 3: printf("Enter element to insert and postion: ");
 					scanf("%d %d", &a, &pos);
 					insertPos(a, pos);
-					break;	
+					break;	*/
 			case 4:	deleteFront();
 					break;
 			case 5:	deleteRear();
 					break;
-			case 6: printf("Enter positon of node: ");
+/*			case 6: printf("Enter positon of node: ");
 					scanf("%d", &pos);
 					deletePos(pos);
 					break;*/
